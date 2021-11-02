d9b4461c28126828d1b1691a6160adf38c5893d2
dmechenko <dmechenko@gmail.com>
2021-11-02 15:50:52 -0400

    Leetcode #1 - Two Sum

---
+++ DSNA/Leetcode/twoSum.js
@@ -0,0 +1,31 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order. */
+
+// const twoSum = (nums, target) => { // unoptimized, O(n^2)
+//   let ans = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) ans.push(i, j);
+//     }
+//   }
+//   return ans;
+// };
+
+const twoSum = (nums, target) => {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+
+  return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));

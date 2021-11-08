c006439ccd3d67f19d3d39af3c25ad557c266af7
dmechenko <dmechenko@gmail.com>
2021-11-08 15:56:29 -0500

    Create Backend Examples directory

--- Backend Examples/backendIndexExample.js
+++ Backend Examples/backendIndexExample.js
@@ -1,4 +1,12 @@
 import express from 'express';
+
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server is alive'));

95f5b9e44186de1f7bf032f4a6c39e2f88575d41
dmechenko <dmechenko@gmail.com>
2021-11-09 16:34:04 -0500

    Setting up new environment

--- DSNA/Leetcode/twoSum.js
+++ DSNA/Leetcode/twoSum.js
@@ -26,6 +26,20 @@ const twoSum = (nums, target) => {
   return [];
 };
 
+const twoProduct = (nums, target) => {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target / nums[i])) {
+      return true;
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return false;
+};
+
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
+console.log(twoProduct([2, 3, 4, 5], 11));

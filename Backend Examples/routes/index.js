e154cafc38cea73f8c0e084ca11fc11b59045870
dmechenko <dmechenko@gmail.com>
2021-11-23 22:01:29 -0500

    inertia

--- Backend Examples/routes/index.js
+++ Backend Examples/routes/index.js
@@ -5,3 +5,5 @@ const router = express.Router();
 router.get('/', (req, res) => {});
 router.post('/', (req, res) => {});
 router.delete('/', (req, res) => {});
+
+//inertia will take care of this if using rails
